b288ff614b6bcd7278a47ef9f144b1a9da8e3465
Luna Genkinger <genkinger@protonmail.com>
2023-01-25 18:16:02 +0100

    Hopefully this fixes the gitignore

--- Monolith/common.py
+++
@@ -1,8 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-from fnmatch import fnmatch
-
-
-def get_file_paths_with_extension(path, extension):
-    return [join(path, file_name) for file_name in listdir(path) if
-            (isfile(join(path, file_name)) and fnmatch(file_name, f"*.{extension}"))]
